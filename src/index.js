d56fa9732b66a76cecf76b11e24958f07d010baa
bello682 <belloadetayo14@gmail.com>
2024-12-25 04:17:23 +0100

    updated

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
 import { BrowserRouter as Router } from "react-router-dom";
 import store from "./store/store";
+import "modern-normalize/modern-normalize.css";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
 	<React.StrictMode>
